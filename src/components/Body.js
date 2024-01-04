2e725b47a46800e0f23a04a45dc5f13cdee4ab9d
SRIDHARALAPATI <sridharalapati22@gmail.com>
2024-01-04 01:24:51 -0600

    React useState Hooks

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,10 @@
 import RestaurantCard from "./RestaurantCard";
 import restaurantList from "../../utils/mockData";
+import { useState } from "react";
 const Body = () => {
+
+ 
+const[resdata, setresdata]=useState(restaurantList);
     return (
       <div className="body-container">
         <div className="search">
@@ -10,9 +14,16 @@ const Body = () => {
             placeholder="Search for restaurant and food"
           />
           <button>Search</button>
+          <button
+            className="filter"
+            onClick={() => {const filterres=resdata.filter((res)=>res.info.avgRating>4.5);
+            setresdata(filterres);
+            }}>
+            Filter
+          </button>
         </div>
         <div className="card-container">
-          {restaurantList.map((resinfo) => (
+          {resdata.map((resinfo) => (
             <RestaurantCard key={resinfo.info.id} resdata={resinfo} />
           ))}
         </div>
